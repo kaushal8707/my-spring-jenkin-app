e39214280e26fabbbe332db22d25fb7d5f2cbe74
Kaushal Kumar <kaukumar3@publicisgroupe.net>
2023-06-13 12:38:21 +0530

    test commit

--- src/test/java/com/java/selfdeveloped/cloud/jenkins/api/MySpringJenkinAppApplicationTests.java
+++ src/test/java/com/java/selfdeveloped/cloud/jenkins/api/MySpringJenkinAppApplicationTests.java
@@ -1,13 +1,23 @@
 package com.java.selfdeveloped.cloud.jenkins.api;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class MySpringJenkinAppApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(MySpringJenkinAppApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true, true);
 	}
 
 }
