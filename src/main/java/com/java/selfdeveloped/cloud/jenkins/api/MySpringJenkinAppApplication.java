d2f560fa947fe7d2135cad48483f8fa22473b808
Kaushal Kumar <kaukumar3@publicisgroupe.net>
2023-06-13 12:05:06 +0530

    first commit

---
+++ src/main/java/com/java/selfdeveloped/cloud/jenkins/api/MySpringJenkinAppApplication.java
@@ -0,0 +1,33 @@
+package com.java.selfdeveloped.cloud.jenkins.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+@RestController
+public class MySpringJenkinAppApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(MySpringJenkinAppApplication.class);
+
+	@GetMapping("/msg")
+	public String message() {
+		return "Wao!! Application Deployed successfully in SAP Cloud..";
+	}
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started !!");
+
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringJenkinAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/selfdeveloped/cloud/jenkins/api/MySpringJenkinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.selfdeveloped.cloud.jenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringJenkinAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
